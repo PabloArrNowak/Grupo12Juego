aa22291bcd8b3363acc07a11ef285fe70a0ba590
Javier Cano Salcedo <jacano01@ucm.es>
2021-04-12 12:46:17 +0200

    Añadida nueva clase Cuenta

---
+++ Practica4MAP/Practica4MAP/Cuenta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practica4MAP
+{
+    class Cuenta
+    {
+        string nombre;
+        int dinero;
+
+        Cuenta (string name, int cash)
+        {
+            nombre = name;
+            dinero = cash;
+        }
+    }
+}
